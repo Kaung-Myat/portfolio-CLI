f4da6cfcffa8c655a6bf8eaa1c6e03b329956f41
Kaung Myat Thu <116712118+Kaung-Myat@users.noreply.github.com>
2023-03-25 12:00:05 -0700

    I redo responsive and add some features

--- script.js
+++ script.js
@@ -17,11 +17,24 @@ $(document).ready(function(){
                 document.getElementById("description").innerText = "";
                 document.getElementById("inputData").value = "";
 
-            }else if(document.getElementById("inputData").value == "portfolio"){
-                window.location.replace('https://kaungmyatthugui.vercel.app');
+            }else if(document.getElementById("inputData").value == "gui"){
+                window.open('https://kaungmyatthugui.vercel.app','_blank');
                 document.getElementById("inputData").value = "";
 
-            }else{
+            }else if(document.getElementById("inputData").value == "socials"){
+                socials();
+                document.getElementById("inputData").value = "";
+            }else if(document.getElementById("inputData").value == "use 1"){
+                window.open("https://github.com/Kaung-Myat","_blank");
+                document.getElementById("inputData").value = "";
+            }else if(document.getElementById("inputData").value == "use 2"){
+                window.open('https://www.facebook.com/profile.php?id=100048568642212','_blank');
+                document.getElementById("inputData").value = "";
+            }else if(document.getElementById("inputData").value == "use 3"){
+                window.open('https://www.instagram.com/kaungmyatthu_tharthar/','_blank');
+                document.getElementById("inputData").value = "";
+            }
+            else{
                 document.getElementById("description").innerHTML +=`
                 <p id="aboutDesc" class="w-75" style="color: white;">
                     '${document.getElementById("inputData").value}' is not recognized.
@@ -46,12 +59,26 @@ function help(){
     var aboutDesc = document.getElementById("description");
     aboutDesc.innerHTML += `
     <ul style="color:white" class="w-75">
-        <li>help --> you can check what you want to do</li>
-        <li>about --> you can check about me</li>
-        <li>portfolio --> you can check my portfolio</li>
-        <li>clear --> you can clear all text</li>
+        <li>help --> To check what you want to do</li>
+        <li>about --> To check about me</li>
+        <li>gui --> To check my GUI portfolio</li>
+        <li>clear --> To clear all text</li>
+        <li>socials --> To check my social media accounts</li>
     </ul>
     `;
+}
+
+function socials(){
+    var aboutDesc = document.getElementById("description");
+    aboutDesc.innerHTML += `
+        <ol>
+            <li>GitHub</li>
+            <li>FaceBook</li>
+            <li>Instagram</li>
+        </ol>
+        <h6>Usage: use (social no) </h6>
+        <p>eg.use 1</p>
+    `
 }
 
 
